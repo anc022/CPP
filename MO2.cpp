da3f3466dd125e26c2ae984b7a0244be8ecd8823
anc022 <77502222+anc022@users.noreply.github.com>
2025-03-21 14:52:13 +0800

    Add files via upload
    Обновил код, добавил функцию для вычисления площади

--- MO2.cpp
+++ MO2.cpp
@@ -1,22 +1,30 @@
 #include <iostream>
-#include <math.h>
+#include <cmath>
 using namespace std;
 
+double Area(double A, int N) {
+    if (N < 3 || A <= 0) {
+        cerr << "Error: The number of sides must be at least 3, and the side length must be positive." << endl;
+        return -1;
+    }
+    double S = (N * A * A) / (4 * tan(M_PI / N));
+    return S;
+}
+
 int main() {
-    double A,S;
+    double A;
     int N;
-    cout << "Enter the length of the sides: ";
+
+    cout << "Enter the length of the sides(A): ";
     cin >> A;
-    cout << "Enter the number of sides: ";
+    cout << "Enter the number of sides(N): ";
     cin >> N;
 
-    if (N < 3 || A <=0){
-        cerr << "Error: The number of sides must be at least 3, and the side length must be positive.";
-        return 1;
-    }
+    double S = Area(A, N);
 
-    S = (N * A * A) / (4 * tan(M_PI / N));
-    cout << "The area is equal to: " << S;
+    if (S != -1) {
+        cout << "Area is: " << S;
+    }
 
     return 0;
 }
